5477d5cbf7540771c6b2eb2685b1914a8daf29a1
itsnowkim <peterhyunjae@naver.com>
2023-03-26 00:00:30 +0900

    done

--- code/itsnowkim/week3/boj_2240.py
+++ code/itsnowkim/week3/boj_2240.py
@@ -8,38 +8,31 @@ T, W = map(int, input().split())
 array = []
 for i in range(T):
     array.append(int(input()))
+array = [0] + array
 
-# dp table
-dp = []
-sol = []
-for i in range(W+1):
-    dp.append([[0,0]]*(T))
-    sol.append([0]*T)
+dp = [[0 for _ in range(W + 1)] for _ in range(T+1)]
 
-# 0번째 초기화
-for i in range(1, T):
-    dp[0][i][0] = 1 # 현재 위치
-    dp[0][i][1] = 0 # 지금까지 이동 횟수
-    
+for i in range(1, T+1):
+    # 맨 첫 줄 초기화
     if array[i] == 1:
-        sol[0][i] = sol[0][i-1] + 1
+        dp[i][0] = dp[i-1][0] + 1
     else:
-        sol[0][i] = sol[0][i-1]
-
-# 이동횟수 1부터 dp 테이블 작성
-for i in range(1, W):
-    pos = 1
-    for j in range(T):
-        if array[j] == 1 and j%2 == 0:
-            sol[i][j] = max(sol[i-1][j], sol[i-1][j-1]) + 1
-        elif array[j] == 2 and j%2 == 1:
-            sol[i][j] = max(sol[i-1][j], sol[i-1][j-1]) + 1
+        dp[i][0] = dp[i-1][0]
+    
+    for j in range(1, min(W+1, i+1)):
+        if j%2 == 0:
+            curr = 1
         else:
-            sol[i][j] = max(dp[i-1][j],dp[i-1][j-1])
+            curr = 2
+        
+        if array[i] == curr:
+            score = 1
+        else:
+            score = 0
 
-print(sol[W][-1])
+        dp[i][j] = max(dp[i-1][j] + score, dp[i-1][j-1] + score)
 
-
-            
+print(max(dp[T]))
+        
                 
 
