dcc41800152d3d945d6ba6b244f6e65d5f9dbf68
itsnowkim <peterhyunjae@naver.com>
2023-03-31 18:58:44 +0900

    공부 끝

---
+++ code/itsnowkim/week4/팀결성.py
@@ -0,0 +1,25 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int,input().split())
+parent = [i for i in range(n+1)]
+
+for i in range(m):
+    oper, a, b = map(int, input().split())
+    if oper == 0:
+        union_parent(parent, a, b)
+    elif oper == 1:
+        if find_parent(parent,a) == find_parent(parent,b):
+            print('YES')
+        else:
+            print('No')
